ba0b57e280ef3dd8034b99cd48592a2b05d65917
Sergej <miron575@yahoo.com>
2024-11-09 13:26:26 +0100

    add heapcheck

--- cpart.cpp
+++ cpart.cpp
@@ -1,7 +1,55 @@
 ﻿#include <stdio.h>
+#include <malloc.h>
+#include <conio.h>
 
 extern bool CudaWork(int *c, const int *a, const int *b, unsigned int size);
 extern bool CudaClose();
+#define PLATFORM_GETCH _getch
+
+void TestHeap(void)
+{
+   // Check heap status, source:
+   // https://learn.microsoft.com/en-us/cpp/c-runtime-library/reference/heapwalk?view=msvc-170
+   _HEAPINFO hinfo;
+   int heapstatus;
+   int numLoops;
+   hinfo._pentry = NULL;
+   numLoops = 0;
+   while ((heapstatus = _heapwalk(&hinfo)) == _HEAPOK)
+   {
+      // printf("%8s block at %Fp of size %4.4X\n",
+      //    (hinfo._useflag == _USEDENTRY ? "USED" : "FREE"),
+      //    hinfo._pentry, hinfo._size);
+      numLoops++;
+   }
+
+   switch (heapstatus)
+   {
+   case _HEAPEMPTY: //printf("OK - empty heap\n");
+      break;
+   case _HEAPEND:// 
+      printf("Heapcheck OK\n");
+      break;
+   case _HEAPBADPTR:
+      printf("ERROR - bad pointer to heap\n");
+      break;
+   case _HEAPBADBEGIN:
+      printf("ERROR - bad start of heap\n");
+      break;
+   case _HEAPBADNODE:
+      printf("ERROR - bad node in heap\n");
+      break;
+   }
+}
+
+void DoSelfTests()
+{
+   //sample_main_PlayBin();
+   //sample_main_SolveBoard();
+   //sample_main_SolveBoard_S1();
+   //sample_main_JK_Solve();
+   TestHeap();
+}
 
 int main()
 {
@@ -16,9 +64,12 @@ int main()
    }
 
    CudaClose();
+
+   TestHeap();
+   PLATFORM_GETCH();
+
    return 0;
 }
-
 
 void IncInCPP(int* dest)
 {
