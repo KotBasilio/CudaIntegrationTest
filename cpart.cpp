cb970ded1f118aca2b0981cfa03f3d50b867fa9e
Sergej <miron575@yahoo.com>
2024-11-09 13:07:02 +0100

    Add project files.

---
+++ cpart.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+extern bool CudaWork(int *c, const int *a, const int *b, unsigned int size);
+extern bool CudaClose();
+
+int main()
+{
+   const int arraySize = 5;
+   const int a[arraySize] = { 1, 2, 3, 4, 5 };
+   const int b[arraySize] = { 10, 20, 30, 40, 50 };
+   int c[arraySize] = { 0 };
+
+   if (CudaWork(c, a, b, arraySize)) {
+      printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
+         c[0], c[1], c[2], c[3], c[4]);
+   }
+
+   CudaClose();
+   return 0;
+}
+
+
+void IncInCPP(int* dest)
+{
+   (*dest)++;
+}
+
